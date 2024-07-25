f04be039c708d5b63ef4b540079b86be58f06f0a
Juanita Mosquera Santos <100717152+JUANITA16@users.noreply.github.com>
2024-07-24 21:36:32 -0500

    Delete pasivos-procesos-minmambu-backend directory

--- pasivos-procesos-minmambu-backend/tests/conftest.py
+++
@@ -1,8 +0,0 @@
-import os
-import pytest
-
-@pytest.fixture(autouse=True, scope="session")
-def env_setup():
-    os.environ["origURL"] = "test-url"
-    os.environ["basePath"] = "/super"
-    os.environ["sapBucket"] = "s3-test-bucket"
